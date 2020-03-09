a87e67c65168f2b3666264b1f356768234e80772
15176 <tongliuzhu@csvw.com>
2020-03-09 04:38:23 -0400

    serial pid

---
+++ build/CMakeFiles/serialpid.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serialpid.dir/src/main.cpp.o"
+  "CMakeFiles/serialpid.dir/src/PID.cpp.o"
+  "serialpid.pdb"
+  "serialpid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serialpid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
